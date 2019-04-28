211c4eb9eb729db258b20d3ea6eae6b807da6d3c
Tushar <gc_tushar@yahoo.com>
2019-04-28 03:06:26 -0400

    test method modified and add method added on calculator

--- generics/example-test/genericstest/src/main/java/org/practice/example/app/Calculator.java
+++ generics/example-test/genericstest/src/main/java/org/practice/example/app/Calculator.java
@@ -1,4 +1,23 @@
 package org.practice.example.app;
 
 public class Calculator {
+
+    public <T extends Number> T add(T first, T second){
+
+            if (first.getClass().equals(Long.class)){
+                return (T) (Long)(first.longValue() + second.longValue());
+            }else if (first.getClass().equals(Integer.class)){
+                return (T) (Integer)(first.intValue() + second.intValue());
+            }else if (first.getClass().equals(Short.class)){
+                return (T) (Short)((short)(first.shortValue() + second.shortValue()));
+            }else if (first.getClass().equals(Integer.class)){
+                return (T) (Byte)((byte)(first.byteValue() + second.byteValue()));
+            }else if (first.getClass().equals(Double.class)){
+                return (T) (Double)(first.doubleValue() + second.doubleValue());
+            }else if (first.getClass().equals(Float.class)){
+                return (T) (Float)(first.floatValue() + second.floatValue());
+            }else {
+                return null;
+            }
+    }
 }
